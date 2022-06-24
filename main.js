1765b66abcb540a360e04253feda0e77c343b935
Danish <dk665226@gmail.com>
2022-06-24 16:02:47 +0500

    Completed The 5 round Game

--- main.js
+++ main.js
@@ -13,18 +13,32 @@ function playRound(playerSelection, computerSelection){
         (playerSelection === 'paper' && computerSelection === 'rock')||
         (playerSelection === 'scissors' && computerSelection === 'paper')){
 
-            playerScore += 1;
+            playerScore ++;
             return result = "You Win! " + playerSelection + ' beats ' + computerSelection + '.' + " Player Score: " + playerScore + "  Computer Score: " + computerScore;
+
     }else if(playerSelection === computerSelection){
         return result = "It's a tie! You both Chose " + playerSelection;
-    
+        
      }else{
-        computerScore += 1; 
+        computerScore ++; 
         return result = "You Lose! " + computerSelection + ' beats ' + playerSelection + '.' + "Player Score: " + playerScore + " Computer Score: " + computerScore;
-     };
+        
+        
+};
 }
+function game(){
+    for (let i = 0; i < 5; i++) {
+        let computerSelection = computerPlay();
+        const pChoose = prompt("Enter Rock/Paper/Scissors")
+        let playerSelection = pChoose.trim().toLowerCase();
+        console.log(playRound(playerSelection, computerSelection));
+}
+if (playerScore > computerScore){
+    return "You Won! Reload the Page to play again."
+}if (playerScore < computerScore){
+    return "You Lost! Reload the Page to play again."
+};
+};
 
-let computerSelection = computerPlay();
-const pChoose = prompt("Enter Rock/Paper/Scissors")
-let playerSelection = pChoose.trim().toLowerCase();
-console.log(playRound(playerSelection, computerSelection));
+
+console.log(game());
